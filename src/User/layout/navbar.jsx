0af35d0be6b990b5e20cd73ecd8355b1005b12ce
AyxanAmirov <amirovayxan0@gmail.com>
2024-07-23 15:18:31 -0700

    route update

--- src/User/layout/navbar.jsx
+++ src/User/layout/navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Logo from "../assets/images/logo.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navBody relative w-full bg-[#B3D6D0] py-[40px] px-[60px]">
@@ -15,10 +16,10 @@ function Navbar() {
             <div className="flex justify-between flex-col items-end w-full gap-[20px]">
               <ul className="flex w-full justify-between items-center">
                 <li className="text-white text-[18px] font-[600] uppercase ">
-                  Home
+                  <Link to="/">Home</Link>
                 </li>
                 <li className="text-white text-[18px] font-[600] uppercase">
-                  About us{" "}
+                  <Link to="/about">About us</Link>
                 </li>
                 <li className="text-white text-[18px]  font-[600] uppercase">
                   Services
@@ -41,11 +42,17 @@ function Navbar() {
                 </li>
               </ul>
               <form className="flex">
-                <input type="text"  className="rounded-l-[10px] rounded-bl-[10px] p-[10px] h-[50px] w-[250px]" placeholder="Search..."/>
+                <input
+                  type="text"
+                  className="rounded-l-[10px] rounded-bl-[10px] p-[10px] h-[50px] w-[250px]"
+                  placeholder="Search..."
+                />
                 <div className="bg-black px-[20px] rounded-r-[10px] rounded-br-[10px] flex items-center">
-                <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white"/>
+                  <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    className="text-white"
+                  />
                 </div>
-
               </form>
             </div>
           </div>
