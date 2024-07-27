4ffee192c246756aaa73c9c9edae838e38ced105
AyxanAmirov <amirovayxan0@gmail.com>
2024-07-27 15:07:56 -0700

    responsive update home 2

--- src/User/pages/home/home.jsx
+++ src/User/pages/home/home.jsx
@@ -8,7 +8,7 @@ import HomeBanner from "../../assets/images/facial-banner.jpg";
 import FacialTitle from "../../assets/images/facial-title.png";
 import Products from "../../components/products/products.jsx";
 import { Link } from "react-router-dom";
-import Slider from "../../components/slider/slider.jsx"
+import Slider from "../../components/slider/slider.jsx";
 import axios from "axios";
 //home slider
 const slides = [
@@ -66,7 +66,7 @@ function Home() {
       <div className="w-full h-[500px] bg-walpaper bg-no-repeat bg-center bg-cover mb-[60px]"></div>
       <div className="container">
         <div className="flex  w-full justify-center">
-          <div className="col-lg-9  ">
+          <div className="col-lg-9 col-md-9 col-sm-9 col-9 ">
             <div className="flex w-full justify-between py-[30px]">
               <img src={CosmeticMarka1} alt="dmg" className="object-contain" />
               <img
@@ -95,22 +95,30 @@ function Home() {
       </div>
       <div className="flex mt-[30px]">
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-          <img src={HomeBanner} alt="banner" className="w-full h-full object-cover"/>
+          <img
+            src={HomeBanner}
+            alt="banner"
+            className="w-full h-full object-cover"
+          />
         </div>
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-          <div className="w-full flex items-center flex-col text-center ">
-            <div className="max-w-[350px] flex flex-col items-center gap-[30px]">
-              <img src={FacialTitle} alt="title" className="xl:w-[100%] lg:w-[100%] md:w-[220px]"/>
-              <h4 className="text-[#393939] font-bold text-[20px] my-[15px]">
+          <div className="w-full flex items-center flex-col text-center md:gap-[20px]">
+            <div className="xl:max-w-[350px] lg:max-w[350px] md:max-w-[350px] sm:max-w-[260px] max-w-[200px] flex flex-col items-center xl:gap-[30px] lg:gap-[30px] md:gap-[20px]">
+              <img
+                src={FacialTitle}
+                alt="title"
+                className="xl:w-[100%] lg:w-[100%] md:w-[220px] sm:w-[220px] w-[150px]"
+              />
+              <h4 className="text-[#393939] font-bold xl:text-[20px] lg:text-[20px] md:text-[20px] sm:text-[16px] text-[14px] my-[15px]">
                 MAKING YOU HAPPY YOU’RE IN YOUR OWN SKIN.
               </h4>
-              <p className="font-bold">
+              <p className="font-bold xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[14px] text-[12px]">
                 Taking care of your skin is a journey. And we’re here to make it
                 a little easier for you. Book your complimentary consultation to
                 get expert advice on the right treatment for you.
               </p>
             </div>
-            <button className="w-full p-[10px] bg-[#f0c6cf] hover:bg-[#EDE1D4] duration-[.3s]">
+            <button className="w-full xl:p-[10px] lg:p-[10px] md:p-[10px] sm:p-[5px] bg-[#f0c6cf] hover:bg-[#EDE1D4] duration-[.3s] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[14px] text-[12px] ">
               <Link>Book Now</Link>
             </button>
           </div>
